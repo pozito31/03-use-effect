5a3b1e9ec49b60e8a56c97c627a0f8b0e055e373
pozito31 <jessicapozo198715@gmail.com>
2023-05-09 16:55:51 +0200

    subir a github

---
+++ src/components/AvisoComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const AvisoComponent = () => {
+    return (
+        <div>
+            <h3>Hemos superado los 20 cambios en el contador</h3>
+            <button>Mostrar alerta</button>
+        </div>
+    );
+}
---
+++ src/components/PruebasComponent.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { AvisoComponent } from './AvisoComponent';
+
+export const PruebasComponent = () => {
+
+    const [usuario, setUsuario] = useState("Jessica Pozo");
+    const [fecha, setFecha] = useState("01-01-1998");
+    const [contador, setContador] = useState(0);
+
+    const modUsuario = e => {
+        setUsuario(e.target.value);
+    }
+
+    const cambiarFecha = e => {
+        setFecha(new Date().toLocaleDateString());
+    }
+
+    //Solo se ejecuta una vez, solo al cargar el componente
+    useEffect(() => {
+        console.log("Has cargado el componente PruebasComponent !!!");
+    }, []);
+
+     //Se ejecuta solo si cambio el usuario
+    useEffect(() => {
+        setContador(contador + 1);
+        console.log("Has modificado el usuario: " + contador);
+        
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [usuario]);
+
+
+    return (
+        <div>
+            <h1>El efecto - Hook useEffect</h1>
+            <strong className={contador >= 10 ? 'label label-green' : 'label'}>{usuario}</strong>
+            <strong>{fecha}</strong>
+            <p>
+                <input type="text"
+                    onChange={modUsuario}
+                    placeholder="Cambia el nombre"
+                />
+                <button onClick={cambiarFecha}>Cambiar fecha</button>
+            </p>
+
+            {contador >= 20 && <AvisoComponent />}
+        </div>
+    );
+}
